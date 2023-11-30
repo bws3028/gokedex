c1cecbdcf3e6bfd9316d1e45edae00bffa6b75fe
Ben Stevenson <bws3028@rit.edu>
2023-11-30 18:00:50 -0500

    Basic commands and user input parsing added

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var cmdList map[string]cliCommand
+
+func init() {
+	cmdList = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Shows all commands",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Gokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+func commandHelp() {
+	for _, cmd := range cmdList {
+		fmt.Printf("%s:\t%s\n", cmd.name, cmd.description)
+	}
+	fmt.Println("")
+}
+
+func commandExit() {
+	os.Exit(0)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Welcome to the Gokedex!")
+	
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("Gokedex> ")
+		input, err := reader.ReadString('\n')
+		input = input[:len(input)-2]
+
+		if err != nil {
+			fmt.Printf("Error occured while reading input\n\n")
+			continue
+		}
+		
+		if cmd, ok := cmdList[input]; ok {
+			cmd.callback()
+		} else {
+			fmt.Printf("Error: %s not a valid command\n\n", input)
+		}
+	}
+
 }
